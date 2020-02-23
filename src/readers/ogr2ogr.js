cba987deb88b658111f4461507b1bc7c4c13f8db
Jim McAndrew <jim@loc8.us>
2020-02-23 10:33:26 -0500

    Return only a stream now

--- src/readers/ogr2ogr.js
+++ src/readers/ogr2ogr.js
@@ -1,33 +1,9 @@
 const ogr2ogr = require('ogr2ogr');
 
 module.exports = function(options) {
-
-  var readStream;
-  var callbackPromise = {};
-  var promise = new Promise((
-    res,
-    rej) => {
-    callbackPromise.res = res;
-    callbackPromise.rej = rej;
-  });
-
-  return {
-    'open': function() {
-      readStream = ogr2ogr(options.connectionString)
-        .options(options.ogrOptions || [])
-        .format('GeoJSON')
-        .stream();
-
-      // console.log(readStream);
-      // readStream.pipe(process.stdout);
-      readStream.on('error', e => callbackPromise.rej(e));
-      readStream.on('end', d => callbackPromise.res(d));
-    },
-    'stream': function() {
-      return readStream;
-    },
-    'promise': function() {
-      return promise;
-    }
-  };
+  return ogr2ogr(options.connectionString)
+    .options(options.ogrOptions || [])
+    .format('GeoJSONSeq')
+    .timeout(options.timeout || 15000)
+    .stream();
 };
