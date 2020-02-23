1e6b51323bd6efa92002a2cc51871bc595d6d9bf
Jim McAndrew <jim@loc8.us>
2020-02-23 10:33:06 -0500

    Make reader more generic, so readers only need to provide a stream now

--- src/reader.js
+++ src/reader.js
@@ -1,5 +1,30 @@
-//TODO MORE!
+const types = {
+  'ogr2ogr': require('./readers/ogr2ogr')
+};
+const Stream = require('stream');
 
-module.exports = function(type) {
-  return require('./readers/' + type);
+module.exports = function(options) {
+
+  var readStream = new Stream.Writable();
+  var callbackPromise = {};
+  var promise = new Promise((
+    res,
+    rej) => {
+    callbackPromise.res = res;
+    callbackPromise.rej = rej;
+  });
+
+  return {
+    'open': function() {
+      readStream = types[options.type](options);
+      readStream.on('error', e => callbackPromise.rej(e));
+      readStream.on('end', d => callbackPromise.res(d));
+    },
+    'stream': function() {
+      return readStream;
+    },
+    'promise': function() {
+      return promise;
+    }
+  };
 };
