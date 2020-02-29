8d4f57876cfb43bac7473644b787786f01431ce5
Jim McAndrew <jim@loc8.us>
2020-02-29 10:31:32 -0500

    Fix typo

--- src/readers/esriRest.js
+++ src/readers/esriRest.js
@@ -5,7 +5,7 @@ const terraformer = require('terraformer-arcgis-parser');
 const Stream = require('stream');
 
 module.exports = function(options) {
-  options['feature-count'] = isNaN(parseInt(options['feature-count', 10])) ? undefined : parseInt(options['feature-count'], 11);
+  options['feature-count'] = isNaN(parseInt(options['feature-count'], 10)) ? undefined : parseInt(options['feature-count'], 10);
   /* Options
    * * connectionString: URL to the resource
    * * options:
